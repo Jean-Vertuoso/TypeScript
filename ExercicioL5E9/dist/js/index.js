31dff537997e31dd6e75628a0c2ec43428e7cdd6
Jean da Rocha Vertuoso <jean_b123@hotmail.com>
2022-07-05 08:11:26 -0300

    add inicio exec l5 e9

---
+++ ExercicioL5E9/dist/js/index.js
@@ -0,0 +1,5 @@
+"use strict";
+//--------------------------------------------------------------- \\ Declaração de variáveis // ---------------------------------------------------------------\\
+var entradaTela = document.getElementById("idIn");
+console.log();
+//-------------------------------------------- \\ Função para alimentar as arrays com informações do usuário // -------------------------------------------------\\
---
+++ ExercicioL5E9/src/index.ts
@@ -0,0 +1,7 @@
+//--------------------------------------------------------------- \\ Declaração de variáveis // ---------------------------------------------------------------\\
+
+var entradaTela = document.getElementById("idIn") as HTMLInputElement; 
+console.log()
+
+//-------------------------------------------- \\ Função para alimentar as arrays com informações do usuário // -------------------------------------------------\\
+
