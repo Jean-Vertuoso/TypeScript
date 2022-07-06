62ecad41cac6f77b45b1fa45660930215171d491
Jean da Rocha Vertuoso <jean_b123@hotmail.com>
2022-07-06 01:32:14 -0300

    add exercicio l5 e9 corrigido

--- ExercicioL5E9/dist/js/index.js
+++ ExercicioL5E9/dist/js/index.js
@@ -1,5 +1,48 @@
 "use strict";
-//--------------------------------------------------------------- \\ Declaração de variáveis // ---------------------------------------------------------------\\
-var entradaTela = document.getElementById("idIn");
-console.log();
-//-------------------------------------------- \\ Função para alimentar as arrays com informações do usuário // -------------------------------------------------\\
+var out = document.getElementById("idSaida");
+var bt = document.getElementById("idBtn");
+bt.onclick = function () {
+    let temp = document.getElementById("idTemp");
+    let escalaC = document.getElementById("idCelsius");
+    let escalaF = document.getElementById("idFahrenheit");
+    let escalaK = document.getElementById("idKelvin");
+    if (escalaC.checked) {
+        let tempF = cToF(Number(temp.value));
+        let tempK = cToK(Number(temp.value));
+        out.value = `Temperatura em Celsius: ${temp.value}ºC\nTemperatura em Fahrenheit: ${tempF.toFixed(2)}ºF\nTemperatura em Kelvin: ${tempK.toFixed(2)}ºK`;
+    }
+    else if (escalaF.checked) {
+        let tempC = fToC(Number(temp.value));
+        let tempK = fToK(Number(temp.value));
+        out.value = `Temperatura em Celsius: ${tempC.toFixed(2)}ºC\nTemperatura em Fahrenheit: ${temp.value}ºF\nTemperatura em Kelvin: ${tempK.toFixed(2)}ºK`;
+    }
+    else if (escalaK.checked) {
+        let tempC = kToC(Number(temp.value));
+        let tempF = kToF(Number(temp.value));
+        out.value = `Temperatura em Celsius: ${tempC.toFixed(2)}ºC\nTemperatura em Fahrenheit: ${tempF.toFixed(2)}ºF\nTemperatura em Kelvin: ${temp.value}ºK`;
+    }
+};
+function cToF(temp) {
+    let tempF = (9 * temp + 160) / 5;
+    return tempF;
+}
+function cToK(temp) {
+    let tempK = (temp + 273);
+    return tempK;
+}
+function fToC(temp) {
+    let tempC = (temp - 32) / 1.8;
+    return tempC;
+}
+function fToK(temp) {
+    let tempK = ((temp - 32) / 1.8) + 273;
+    return tempK;
+}
+function kToC(temp) {
+    let tempK = temp - 273;
+    return tempK;
+}
+function kToF(temp) {
+    let tempK = ((temp - 273) * 1.8) + 32;
+    return tempK;
+}
--- ExercicioL5E9/src/index.ts
+++ ExercicioL5E9/src/index.ts
@@ -1,7 +1,53 @@
-//--------------------------------------------------------------- \\ Declaração de variáveis // ---------------------------------------------------------------\\
+var out = document.getElementById("idSaida") as HTMLOutputElement; 
+var bt = document.getElementById("idBtn") as HTMLButtonElement;
 
-var entradaTela = document.getElementById("idIn") as HTMLInputElement; 
-console.log()
+bt.onclick = function(){
+    let temp    = document.getElementById("idTemp") as HTMLInputElement;
+    let escalaC = document.getElementById("idCelsius") as HTMLInputElement; 
+    let escalaF = document.getElementById("idFahrenheit") as HTMLInputElement; 
+    let escalaK = document.getElementById("idKelvin") as HTMLInputElement; 
 
-//-------------------------------------------- \\ Função para alimentar as arrays com informações do usuário // -------------------------------------------------\\
+    if (escalaC.checked) {
+       let tempF: number =  cToF(Number(temp.value));
+       let tempK: number =  cToK(Number(temp.value));
+       out.value = `Temperatura em Celsius: ${temp.value}ºC\nTemperatura em Fahrenheit: ${tempF.toFixed(2)}ºF\nTemperatura em Kelvin: ${tempK.toFixed(2)}ºK` 
+    } else if(escalaF.checked) {
+       let tempC: number = fToC(Number(temp.value));
+       let tempK: number = fToK(Number(temp.value));
+       out.value = `Temperatura em Celsius: ${tempC.toFixed(2)}ºC\nTemperatura em Fahrenheit: ${temp.value}ºF\nTemperatura em Kelvin: ${tempK.toFixed(2)}ºK` 
+    } else if(escalaK.checked) {
+       let tempC: number = kToC(Number(temp.value));
+       let tempF: number = kToF(Number(temp.value));
+       out.value = `Temperatura em Celsius: ${tempC.toFixed(2)}ºC\nTemperatura em Fahrenheit: ${tempF.toFixed(2)}ºF\nTemperatura em Kelvin: ${temp.value}ºK` 
+    }
+}
 
+function cToF(temp: number): number{
+    let tempF: number = (9 * temp + 160)/5;
+    return tempF;
+}
+
+function cToK(temp: number): number{
+    let tempK: number = (temp + 273);
+    return tempK;
+}
+
+function fToC(temp: number): number{
+    let tempC: number = (temp - 32)/1.8;
+    return tempC;
+}
+
+function fToK(temp: number): number{
+    let tempK: number = ((temp - 32)/1.8) + 273;
+    return tempK;
+}
+
+function kToC(temp: number): number{
+    let tempK: number = temp - 273;
+    return tempK;
+}
+
+function kToF(temp: number): number{
+    let tempK: number = ((temp - 273) * 1.8) + 32;
+    return tempK;
+}
